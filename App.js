f88a810106e5cf315fdca95d435f36bb5210e2ac
Reza Nikoopour <rnikoopour@riotgames.com>
2017-08-18 11:38:24 -0700

    Landing page uses flex

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import { StatusBar, Image, StyleSheet, Text, View } from 'react-native';
 
+import { Landing } from './Components/Landing';
+import { MainPage } from './Components/MainPage';
 import { Opponent } from './Components/Opponent';
 
-export default class App extends React.Component {
-    render() {
-	return (
-	    <View style={styles.container}>
-	      <StatusBar hidden={true} />
-	      <Opponent />
-	    </View>
-	);
-    }
-}
-
+const states = {
+    findUser: Symbol(),
+    mainPage: Symbol(),
+    inGame: Symbol()
+};
 
 const styles = StyleSheet.create({
     container: {
 	flex: 1
     }
 });
+
+function findUser(username) {
+    return states.mainPage;
+}
+
+export default class App extends React.Component {
+    constructor() {
+	super();
+	this.views = {
+	    [states.findUser]: (
+		<View style={styles.container}>
+		  <Landing action={_ => this.setState({appState: findUser()})}/>
+		</View>
+	    ),
+	    [states.mainPage]: (
+		<View style={styles.container}>
+		  <MainPage />
+		</View>
+	    )
+	};
+	this.state = {
+	    appState: states.findUser
+	};
+    }
+
+    render() {
+	return this.views[this.state.appState];
+    }
+}
+
+
--- Components/Landing/index.js
+++ Components/Landing/index.js
@@ -1,28 +1,39 @@
 import React, {Component} from 'react';
-import { View, TextInput, Picker, Button } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard, View, TextInput, Picker, Button } from 'react-native';
 
-export default class Landing extends Component {
-	constructor(props) {
- 		super(props);
- 		this.state = {
-    		region: 'br'
-  		};
-	}
+export class Landing extends Component {
+    constructor(props) {
+ 	super(props);
+ 	this.state = {
+    	    region: 'br'
+  	};
+    }
 
-	render() {
-		const regions = ['br','eune', 'euw', 'lan', 'las', 'na', 'oce', 
-			'ru', 'tr', 'jp', 'sea', 'kr', 'pbe'];
+    render() {
+	const regions = ['br','eune', 'euw', 'lan', 'las', 'na', 'oce', 
+			 'ru', 'tr', 'jp', 'sea', 'kr', 'pbe'];
 
-		const regionPickers = regions.map((region, key) => <Picker.Item key={key} label={region} value={region} />); 
+	const regionPickers = regions.map((region, key) => <Picker.Item key={key} label={region} value={region} />); 
 
-		return (
-		<View style={{flex:1, flexDirection: 'column', padding: 100 }}> 
-			<TextInput style={{ height: 40, width: 220 }} placeholder="Summoner Name" />
-			<Picker selectedValue={this.state.region} onValueChange={(itemValue, itemIndex) => this.setState({region: itemValue})}>
-				{regionPickers}
-			</Picker> 
-			<Button title="Go" accessibilityLabel="Press to store this information within the app as your default account" onPress={this.props.action}/>
-		</View> 
-		);
-	}
+	return (
+	    <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{flex:1, flexDirection: 'column' }}> 
+	      <View style={{flex:1, flexDirection: 'column' }}>
+		<View style={{flex:1/3, alignItems:'center'}}>
+		  <View style={{flex:4}}></View>
+		  <View style={{flex:1}}>
+		    <TextInput style={{flex:1, alignSelf: 'flex-end'}} placeholder="Summoner Name" />
+		  </View>
+		</View>
+		<View style={{flex:1/3, flexDirection: 'column' }}>
+		  <Picker style={{flex:1}} selectedValue={this.state.region} onValueChange={(itemValue, itemIndex) => this.setState({region: itemValue})}>
+		    {regionPickers}
+		  </Picker> 
+		</View>
+		<View style={{flex:1/3, flexDirection: 'column' }}>
+		  <Button style={{flex:1}} title="Go" accessibilityLabel="Press to store this information within the app as your default account" onPress={this.props.action}/>
+		</View>
+	      </View>
+	    </TouchableWithoutFeedback>
+	);
+    }
 }
